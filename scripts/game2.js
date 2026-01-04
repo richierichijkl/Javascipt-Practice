2f365670322df149d8edddc289c7e87b16f19a8e
richierichijk <rc28122003@gmail.com>
2026-01-04 10:01:40 +0530

    javascipt-game2

--- scripts/game2.js
+++ scripts/game2.js
@@ -21,17 +21,47 @@ function movements(move){
     }
 
     play(myMove,machineMove);
-    alert(`Your choice : ${myMove} ; Machines : ${machineMove} ; wins:${score.wins} ; losses:${score.losses} ; ties:${score.ties}`);
+    /*alert(`Your choice : ${myMove} ; Machines : ${machineMove} ; wins:${score.wins} ; losses:${score.losses} ; ties:${score.ties}`);*/
+
+
 }
+
+let winElement=document.querySelector('.js-winStatus');
+let scoreElement=document.querySelector('.js-scores');
+let winIconElement=document.querySelector('.js-winIcon');
+winElement.innerHTML="";
+scoreElement.innerHTML="";
+winIconElement.innerHTML="";
+
+let icons={
+    rock:"../Assignments/rock-emoji.png",
+    paper:"../Assignments/paper-emoji.png",
+    scissors:"../Assignments/scissors-emoji.png"
+};
 
 function play(myMove,machineMove){
     if((myMove=='rock' && machineMove=='scissors') || (myMove=='scissors' && machineMove=='paper') || (myMove=='paper' && machineMove=='rock')){
         score.wins++;
+        winElement.innerHTML="You win";
+        
+        
     }else if((myMove=='rock' && machineMove=='paper') || (myMove=='scissors' && machineMove=='rock') || (myMove=='paper' && machineMove=='scissors')){
         score.losses++;
+        winElement.innerHTML="You lost";
+
     }else{
         score.ties++;
+        winElement.innerHTML="Its tie";
     }
+
+    winIconElement.innerHTML = `
+    You 
+    <img src="${icons[myMove]}" class="icon">
+    <img src="${icons[machineMove]}" class="icon">
+    Computer
+  `;
+    scoreElement.innerHTML=`wins:${score.wins} ; losses:${score.losses} ; ties:${score.ties}`;
+    
 
     localStorage.setItem('score',JSON.stringify(score));
 }
@@ -40,5 +70,6 @@ function reset(){
     score.wins=0;
     score.losses=0;
     score.ties=0;
+    scoreElement.innerHTML=`wins:${score.wins} ; losses:${score.losses} ; ties:${score.ties}`;
     localStorage.setItem('score',JSON.stringify(score));
 }
